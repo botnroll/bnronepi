2d9511bbfa3f28f261f4b0bcba3672ae83b44ad8
Nino Pereira <ninopereira.pt@gmail.com>
2023-08-16 23:29:24 +0100

    add battery, buzzer and lcd examples in basic folder

---
+++ onepi/examples/basic/battery.py
@@ -0,0 +1,37 @@
+"""
+Description:
+Read the battery voltage and print its value on the LCD and Serial Monitor.
+Also configures the minimum battery voltage that causes the robot to
+stop if voltage is below the defined value.
+It is VERY important that you define this minimum voltage correctly
+to preserve your robot's battery life.
+"""
+
+import time
+from one import BnrOneA
+
+one = BnrOneA(0, 0)  # declaration of object variable to control the Bot'n Roll ONE A
+
+
+def setup():
+    one.stop()  # stop motors
+    one.min_bat(
+        10.5
+    )  # define de minimum battery voltage. Robot stops if voltage is below the specified value!
+
+
+def loop():
+    battery = one.read_battery()  # read battery voltage
+    one.lcd2("Battery V: ", battery)  # print data on LCD line 2
+    print("Battery V: ", battery)
+    time.sleep(0.2)  # wait 200 milliseconds
+
+
+def main():
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ onepi/examples/basic/buzzer.py
@@ -0,0 +1,44 @@
+"""
+This example code is in the public domain.
+ http://arduino.cc/en/Tutorial/Tone 
+"""
+
+import time
+from pitches import *
+
+# notes in the melody:
+melody =  [note_C4, note_G3, note_G3, note_A3, note_G3, 0, note_B3, note_C4]
+
+# note durations: 4 = quarter note, 8 = eighth note, etc.:
+note_durations =  [4, 8, 8, 4, 4, 4, 4, 4]
+
+def setup():
+
+    # iterate over the notes of the melody:
+    for this_note in range(8):
+        # to calculate the note duration, take one second 
+        # divided by the note type.
+        # e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        note_duration = 1000/note_durations[this_note]
+        tone(9, melody[this_note],note_duration)
+
+        # to distinguish the notes, set a minimum time between them.
+        # the note's duration + 30% seems to work well:
+        pause_between_notes = note_duration * 1.30
+        time.sleep(pause_between_notes)
+        # stop the tone playing:
+        no_tone(9)
+
+
+def loop():
+    pass # no need to repeat the melody.
+
+
+def main():
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == "__main__":
+    main()
