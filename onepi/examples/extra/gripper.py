8101d4ab10af3ecb8876804782af1359804a0782
Nino Pereira <ninopereira.pt@gmail.com>
2023-09-05 23:15:03 +0100

    gripper test OK

--- onepi/examples/extra/gripper.py
+++ onepi/examples/extra/gripper.py
@@ -1,4 +1,6 @@
 """
+ Latest update: 05-09-2023
+
  This code example is in the public domain.
  http://www.botnroll.com
 
@@ -7,15 +9,19 @@ This example demonstrates the use of the Gripper.
 NOTE:
 Gripper1 values vary between  80 - 160 (upwards - downwards) - (130 corresponds to Horizontal)
 Gripper2 values vary between  18 - 120 (closed - open)
+
+On the Raspberry Pi side the output pins are GPIO17 and GPIO18 (corresponding to pins 11 and 12).
+On the Bot'n Roll side the gripper should be connected to pins 2 and 3.
+Press PB1 to increase the angle and PB2 to decrease it.
+Press PB3 to change selection of servo.
 """
 
 import time
 from one import BnrOneA
-
-one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
-
 import RPi.GPIO as GPIO
 import time
+
+one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
 
 GPIO.setmode(GPIO.BCM)
 
@@ -34,7 +40,7 @@ servo = 1
 
 
 def setup():
-    one.stop()  #  stop motors
+    one.stop()  # stop motors
     one.lcd1("Bot'n Roll ONE A")
     one.lcd2("www.botnroll.com")
     time.sleep(1)
@@ -43,9 +49,9 @@ def setup():
     gripper2.start(0)
 
 
-def set_angle(servo, angle):
+def set_angle(servo_motor, angle):
     duty_cycle = 2 + (angle / 18)  # Map angle to duty cycle
-    servo.ChangeDutyCycle(duty_cycle)
+    servo_motor.ChangeDutyCycle(duty_cycle)
 
 
 def gripper_open():
@@ -56,7 +62,19 @@ def gripper_close():
     set_angle(gripper2, 18)
 
 
+def cap_value(value, lower_limit, upper_limit):
+    """
+    Caps the value to lower and upper limits
+    """
+    value = max(value, lower_limit)
+    value = min(value, upper_limit)
+    return value
+
+
 def loop():
+    global pos_servo_1
+    global pos_servo_2
+    global servo
     button = one.read_button()
     if button == 1:
         if servo == 1:
@@ -72,15 +90,12 @@ def loop():
         servo += 1
         if servo > 2:
             servo = 1
+        one.lcd1("   Servo = " + str(servo))
+        one.lcd2("")
+        time.sleep(1)
 
-    if pos_servo_1 > 200:
-        pos_servo_1 = 200
-    if pos_servo_2 > 200:
-        pos_servo_2 = 200
-    if pos_servo_1 < 0:
-        pos_servo_1 = 0
-    if pos_servo_2 < 0:
-        pos_servo_2 = 0
+    pos_servo_1 = cap_value(pos_servo_1, 0, 200)
+    pos_servo_2 = cap_value(pos_servo_2, 0, 200)
 
     one.lcd1("Gripper 1: ", pos_servo_1)
     one.lcd2("Gripper 2: ", pos_servo_2)
@@ -88,6 +103,8 @@ def loop():
         set_angle(gripper1, pos_servo_1)
     elif servo == 2:
         set_angle(gripper2, pos_servo_2)
+    msg = "Gripper 1: " + str(pos_servo_1) + "    Gripper 2: " + str(pos_servo_2) + "    Servo = " + str(servo)
+    print(msg, end="     \r")
     time.sleep(0.2)
 
 
