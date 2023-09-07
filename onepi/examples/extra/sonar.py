fd01aca8eea30fcbae2d8594832b8843db9004bf
Nino Pereira <ninopereira.pt@gmail.com>
2023-09-07 23:18:01 +0100

    sonar test OK

--- onepi/examples/extra/sonar.py
+++ onepi/examples/extra/sonar.py
@@ -1,11 +1,11 @@
 """
- HC-SR04 Ping distance sensor:
- VCC to arduino 5v
- GND to arduino GND
- Echo to Arduino pin 6
- Trig to Arduino pin 7
+ Latest update: 07-09-2023
 
- This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ HC-SR04 Ping distance sensor:
+ VCC to 5v
+ GND to GND
+ Echo to RaspberryPi GPIO5 (BotnRoll pin 6)
+ Trig to RaspberryPi GPIO6 (BotnRoll pin 7)
 
  This code example is in the public domain.
  http://www.botnroll.com
@@ -14,77 +14,49 @@
 import time
 from one import BnrOneA
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+
+GPIO.setmode(GPIO.BCM)  # Use GPIO numbering
 
 one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
 
-echoPin = 5    # 6 on robot side - Echo Pin
-trigPin = 6    # 7 on robot side - Trigger Pin
+echo_pin = 5  # 6 on robot side - Echo Pin
+trigger_pin = 6  # 7 on robot side - Trigger Pin
 
-GPIO.setup(echoPin, GPIO.IN)
-GPIO.setup(trigPin, GPIO.OUT)
-
-maximum_range = 200      # Maximum range (200cm)
-minimum_range = 0        # Minimum range
+GPIO.setup(echo_pin, GPIO.IN)
+GPIO.setup(trigger_pin, GPIO.OUT)
 
 
-def pulseIn(pin, state, timeout_us=1000000):
-    """
-    In this script, the pulseIn function waits for a pulse with a
-    specified state (HIGH or LOW) on the given pin.
-    It returns the duration of the pulse in microseconds.
-    The timeout_us parameter specifies the maximum time to wait for
-    a pulse before giving up.
-    If the timeout is reached, the function returns 0.
-    """
-    start_time = time.time()
-    while GPIO.input(pin) != state:
-        if (time.time() - start_time) * 1e6 > timeout_us:
-            return 0
+def read_sonar():
+    GPIO.output(trigger_pin, True)
+    time.sleep(0.00001)
+    GPIO.output(trigger_pin, False)
 
-    pulse_start = time.time()
-    while GPIO.input(pin) == state:
-        if (time.time() - start_time) * 1e6 > timeout_us:
-            return 0
+    while GPIO.input(echo_pin) == 0:
+        pulse_start = time.time()
 
-    pulse_end = time.time()
-    return int((pulse_end - pulse_start) * 1e6)
+    while GPIO.input(echo_pin) == 1:
+        pulse_end = time.time()
 
+    pulse_duration = pulse_end - pulse_start
+    distance_cm = pulse_duration * 17150
 
-def Sonar():
-    tempo = time.time()
-
-    GPIO.output(trigPin, GPIO.LOW)
-    time.sleep(0.002)
-
-    GPIO.output(trigPin, GPIO.HIGH)
-    time.sleep(0.010)
-
-    GPIO.output(trigPin, GPIO.LOW)
-
-    pin_state = GPIO.input(echoPin)
-    duration = pulseIn(echoPin, GPIO.HIGH, 11640)
-    print(f"Pulse duration: {duration} microseconds")
-
-    time.sleep(0.016 - (time.time() - tempo))  # this routine has fixed time (16 milliseconds)
-
-    # Calculate the distance (in cm) based on the speed of sound
-    distance_cm = int(duration / 58.2)
-    if (distance_cm >= maximum_range or distance_cm <= minimum_range)
-        distance_cm = -1
-    return distance_cm
+    return round(distance_cm, 2)
 
 
 def setup():
-    one.stop()             # stop motors
+    one.stop()  # stop motors
     one.lcd1("www.botnroll.com")
     one.lcd2(" ")
+    GPIO.output(trigger_pin, False)
+    print("Waiting 2s for Sensor To Settle")
+    time.sleep(2)
+    print("Reading...")
 
 
 def loop():
-    distance_cm = Sonar()
-    one.lcd2("distance_cm: ", distance_cm)
-    print(distance_cm)
+    distance_cm = read_sonar()
+    one.lcd2("distance: ", int(distance_cm))
+    print("Distance:", distance_cm, "cm", end="   \r")
     time.sleep(0.050)
 
 
