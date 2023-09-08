54c87d1e5d6289c694d92abefbf694b6d631ba48
Nino Pereira <ninopereira.pt@gmail.com>
2023-09-08 23:47:41 +0100

    buzzer test OK

--- onepi/examples/basic/buzzer.py
+++ onepi/examples/basic/buzzer.py
@@ -1,16 +1,37 @@
 """
-This example code is in the public domain.
- http://arduino.cc/en/Tutorial/Tone
+ Latest update: 08-09-2023
+
+ This code example is in the public domain.
+ http://www.botnroll.com
+
+This example works better by using a software pwm instead of using 
+the hardware pwm pins on the Raspberry Pi.
+Note that the connection to the Bot'n Roll should be on pin 9 (where the buzzer is attached to).
 """
 
 import time
+import RPi.GPIO as GPIO
 from pitches import *
 
 # notes in the melody:
 melody = [note_C4, note_G3, note_G3, note_A3, note_G3, 0, note_B3, note_C4]
-
 # note durations: 4 = quarter note, 8 = eighth note, etc.:
 note_durations = [4, 8, 8, 4, 4, 4, 4, 4]
+
+# Set up GPIO
+BUZZER_PIN = 26
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+
+def tone(pin, frequency, duration):
+    if frequency <= 0:
+        frequency = 1  # inaudible
+
+    p = GPIO.PWM(BUZZER_PIN, frequency)  # Create a PWM object
+    p.start(99)  # 99% duty cycle
+    time.sleep(duration / 1000)
+    p.stop()  # Stop the PWM
 
 
 def setup():
@@ -24,10 +45,8 @@ def setup():
 
         # to distinguish the notes, set a minimum time between them.
         # the note's duration + 30% seems to work well:
-        pause_between_notes = note_duration * 1.30
+        pause_between_notes = note_duration * 1.30 / 1000
         time.sleep(pause_between_notes)
-        # stop the tone playing:
-        no_tone(9)
 
 
 def loop():
@@ -38,6 +57,8 @@ def main():
     setup()
     while True:
         loop()
+    # Clean up GPIO
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
