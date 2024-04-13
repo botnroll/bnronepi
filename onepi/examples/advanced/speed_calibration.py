bbfb91f850b8eaa4dca7934045816b362928c771
Nino Pereira <ninopereira.pt@gmail.com>
2024-04-13 11:04:37 +0100

    fix ticks per rev for speed calibration

--- onepi/examples/advanced/speed_calibration.py
+++ onepi/examples/advanced/speed_calibration.py
@@ -21,9 +21,9 @@ max_linear_speed = 60  # (mm/s)
 speed_conversion_factor = 1  # conversion factor from real speeds to percentage
 file_name = "config_speed_factor.json"
 filename = os.path.join(os.path.dirname(__file__), file_name)
-MAX_SPEED = 100.0
-TICKS_PER_REV = 180  # encoder ticks per revolution
-WHEEL_DIAMETER_MM = 46  # mm
+MAX_SPEED = 50.0
+TICKS_PER_REV = 1500  # encoder ticks per revolution
+WHEEL_DIAMETER_MM = 63  # mm
 
 
 def load_config():
@@ -58,8 +58,10 @@ def save_config(speed_conversion_factor_in):
 
 def determine_speed(speed_in, wheel_diameter, ticks_per_rev, motion_duration_s=1):
     one.stop()
+    one.move(10,10)
+    time.sleep(0.1)
     one.move(speed_in, speed_in)
-    time.sleep(0.2)  # allow enough time for wheels to reach max speed
+    time.sleep(0.5)  # allow enough time for wheels to reach max speed
     one.read_left_encoder()  # reset encoder
     one.read_right_encoder()  # reset encoder
     time.sleep(motion_duration_s)
@@ -67,12 +69,16 @@ def determine_speed(speed_in, wheel_diameter, ticks_per_rev, motion_duration_s=1
     right_count = one.read_right_encoder()
     one.stop()
     average_count = (left_count + right_count) / 2.0
+    print ("average_count: ", average_count)
     revolutions = average_count / ticks_per_rev
     wheel_perimeter = wheel_diameter * math.pi
     distance = wheel_perimeter * revolutions
     speed_mmps = distance / motion_duration_s  #
     one.lcd1("distance ", distance)
     one.lcd2("speed: ", speed_mmps)
+    print("distance: ", distance)
+    print("speed: ", speed_mmps)
+    time.sleep(2)
     return speed_mmps
 
 def calibrate_speed_factor():
@@ -99,28 +105,35 @@ def verify_calibration():
     half_max_speed_mmps = max_speed_mmps / 2.0
     # verify linearity of the speed conversion
     speed_mmps = determine_speed(MAX_SPEED / 2.0, WHEEL_DIAMETER_MM, TICKS_PER_REV)
+    print("half_max_speed_mmps: ", speed_mmps)
     real_speed_tolerance = (tolerance * max_speed_mmps) / 100.0
     error = speed_mmps - half_max_speed_mmps
+    print("error: ", error)
     one.lcd2("error = ", error)
-    if (within_tolerance(speed_mmps, half_max_speed_mmps, real_speed_tolerance))
+    if (within_tolerance(speed_mmps, half_max_speed_mmps, real_speed_tolerance)):
         one.lcd1("Calibration OK")
     else:
         one.lcd1("Calibration Failed")
 
+def wait_user_input():
+    while one.read_button() == 0:  # Wait a button to be pressed
+        pass
+    while one.read_button() != 0:  # Wait for button release
+        pass
+    
 def setup():
     one.min_battery(10.5)  # safety voltage for discharging the battery
     one.stop()  # stop motors
     one.lcd1("Speed calibration")
     one.lcd2(" Press a button ")
-    while one.read_button() == 0:  # Wait a button to be pressed
-        pass
-    while one.read_button() != 0:  # Wait for button release
-        pass
+    #wait_user_input()
     calibrate_speed_factor()
+    wait_user_input()
     verify_calibration()
 
 def loop():
     one.stop()
+    time.sleep(1)
 
 def main():
     setup()
