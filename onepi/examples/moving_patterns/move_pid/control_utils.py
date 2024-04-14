762db4e85dc11282385045fa95555b3fa927e0be
Nino Pereira <ninopereira.pt@gmail.com>
2024-04-14 22:02:54 +0100

    fix control utils

--- onepi/examples/moving_patterns/move_pid/control_utils.py
+++ onepi/examples/moving_patterns/move_pid/control_utils.py
@@ -15,16 +15,27 @@ class ControlUtils:
     _axis_length_mm = 163.0
     _wheel_diameter_mm = 65.0
     _ticks_per_rev = 80
+    _max_speed_mmps = 850
+    _min_speed_mmps = 0
     _spot_rotation_delta = -14.5  # For spot rotations only
     _pi = 3.14159265
 
-    def __init__(self, axis_length_mm, wheel_diameter_mm, ticks_per_rev):
+    def __init__(
+        self,
+        axis_length_mm,
+        wheel_diameter_mm,
+        ticks_per_rev,
+        max_speed_mmps=850,
+        min_speed_mmps=0,
+    ):
         """
         constructor
         """
         self._axis_length_mm = axis_length_mm
         self._wheel_diameter_mm = wheel_diameter_mm
         self._ticks_per_rev = ticks_per_rev
+        self._max_speed_mmps = max_speed_mmps
+        self._min_speed_mmps = min_speed_mmps
 
     def convert_range(self, x_value, x_min, x_max, y_min, y_max):
         """
@@ -47,15 +58,13 @@ class ControlUtils:
            computes the expected number of pulses given the number of revolutions of the wheel
         */
         """
-        global TICKS_PER_REV
-        return float(pulses) / TICKS_PER_REV
+        return float(pulses) / self._ticks_per_rev
 
     def compute_distance_from_rev(self, revolutions):
         """
         computes distance given the number of revolutions
         """
-        global WHEEL_DIAMETER_MM, PI
-        distance_mm = PI * float(WHEEL_DIAMETER_MM) * revolutions
+        distance_mm = self._pi * float(self._wheel_diameter_mm) * revolutions
         return distance_mm
 
     def compute_speed_from_distance(self, distance_mm, time_ms):
@@ -84,8 +93,7 @@ class ControlUtils:
         """
         computes the number of revolutions expected for the wheel for a given distance
         """
-        global PI, WHEEL_DIAMETER_MM
-        perimeter_of_circle = PI * float(WHEEL_DIAMETER_MM)
+        perimeter_of_circle = self._pi * float(self._wheel_diameter_mm)
         revolutions = distance_mm / perimeter_of_circle
         return revolutions
 
@@ -93,13 +101,12 @@ class ControlUtils:
         """
         Computes the arc length given the angle and radius of curvature
         """
-        global AXIS_LENGTH_MM, SPOT_ROTATION_DELTA
         arc_length_mm = 0.0
         if radius_of_curvature_mm != 0.0:
             arc_length_mm = angle_rad * radius_of_curvature_mm
         else:
             arc_length_mm = (
-                angle_rad * float(AXIS_LENGTH_MM + SPOT_ROTATION_DELTA)
+                angle_rad * float(self._axis_length_mm + self._spot_rotation_delta)
             ) / 2.0
         return arc_length_mm
 
@@ -107,15 +114,13 @@ class ControlUtils:
         """
         computes number of ticks from number of revolutions
         """
-        global TICKS_PER_REV
-        return revolutions * TICKS_PER_REV
+        return revolutions * self._ticks_per_rev
 
     def compute_pulses_from_rev(self, revolutions):
         """
         computes the expected number of ticks given the number of revolutions of the wheel
         """
-        global TICKS_PER_REV
-        return round(TICKS_PER_REV * revolutions)
+        return round(self._ticks_per_rev * revolutions)
 
     def compute_ticks_from_speed(self, speed_mmps, time_ms):
         """
@@ -140,22 +145,30 @@ class ControlUtils:
         """
         convert speed percentage to real speed in mmps
         """
-        global MAX_SPEED, MIN_SPEED
         capped_speed = cap_to_limits(desired_speed_percentage, -100, 100)
         if capped_speed < 0:
-            return convert_range(capped_speed, -100, 0, -MAX_SPEED, -MIN_SPEED)
+            return self.convert_range(
+                capped_speed, -100, 0, -self._max_speed_mmps, -self._min_speed_mmps
+            )
         if capped_speed > 0:
-            return convert_range(capped_speed, 0, 100, MIN_SPEED, MAX_SPEED)
+            return self.convert_range(
+                capped_speed, 0, 100, self._min_speed_mmps, self._max_speed_mmps
+            )
         return 0
 
     def convert_to_percentage(self, desired_speed_mmps):
         """
         convert real speed to speed percentage
         """
-        global MAX_SPEED, MIN_SPEED
-        cappedSpeed = cap_to_limits(desired_speed_mmps, -MAX_SPEED, MAX_SPEED)
-        if cappedSpeed <= -MIN_SPEED:
-            return map(cappedSpeed, -MAX_SPEED, -MIN_SPEED, -100, 0)
-        if cappedSpeed >= MIN_SPEED:
-            return map(cappedSpeed, MIN_SPEED, MAX_SPEED, 0, 100)
+        capped_speed = cap_to_limits(
+            desired_speed_mmps, -self._max_speed_mmps, self._max_speed_mmps
+        )
+        if capped_speed <= -self._min_speed_mmps:
+            return self.convert_range(
+                capped_speed, -self._max_speed_mmps, -self._min_speed_mmps, -100, 0
+            )
+        if capped_speed >= self._min_speed_mmps:
+            return self.convert_range(
+                capped_speed, self._min_speed_mmps, self._max_speed_mmps, 0, 100
+            )
         return 0
