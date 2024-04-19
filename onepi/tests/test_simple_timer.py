d905fe68f290158ca2359fb5f9c162ac33e815fd
Nino Pereira <ninopereira.pt@gmail.com>
2024-04-19 23:09:32 +0100

    refactor simple timer test

--- onepi/tests/test_simple_timer.py
+++ onepi/tests/test_simple_timer.py
@@ -1,4 +1,7 @@
 from onepi.utils.simple_timer import SimpleTimer
+
+import os
+import sys
 import time
 
 # these steps are necessary in order to import modules
@@ -6,53 +9,47 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(project_root)
 
+def time_elapsed():
+    pass
 
 def test_start_time():
-    count = 0
-
-    def time_elapsed():
-        count += my_timer.get_time()
-
     my_timer = SimpleTimer(increment=1, function=time_elapsed)
     my_timer.start()
-    assert count == 0
+    assert my_timer.get_time() == 0
     time.sleep(1.2)
-    assert count == 1
+    assert my_timer.get_time() == 1
+    my_timer.stop()
 
-
+def test_stop_time():
+    my_timer = SimpleTimer(increment=1, function=time_elapsed)
+    my_timer.start()
+    assert my_timer.get_time() == 0
+    time.sleep(1.2)
+    my_timer.stop()
+    assert my_timer.get_time() == 1
+    time.sleep(1.2)
+    assert my_timer.get_time() == 1
+    my_timer.start()
+    time.sleep(1.2)
+    assert my_timer.get_time() == 2
+    my_timer.stop()
+    
+def test_reset_time():
+    my_timer = SimpleTimer(increment=1, function=time_elapsed)
+    my_timer.start()
+    assert my_timer.get_time() == 0
+    time.sleep(1.2)
+    my_timer.reset()
+    assert my_timer.get_time() == 0
+    time.sleep(1.2)
+    assert my_timer.get_time() == 0
+    my_timer.start()
+    time.sleep(1.2)
+    assert my_timer.get_time() == 1
+    my_timer.stop()
+    
 def main():
     print("Run tests using: pytest", os.path.basename(__file__), "-s")
-
-    # def time_elapsed():
-    #     print("...elapsed time:", my_timer.get_time())
-
-    # my_timer = SimpleTimer(increment=1, function=time_elapsed)
-
-    # my_timer.start()
-    # time.sleep(5.4)
-
-    # print("Starting timer 2nd time")
-    # my_timer.start()
-    # time.sleep(3)
-    # print("Stopping timer")
-    # my_timer.stop()
-    # time.sleep(3)
-    # print("Starting timer 3rd time")
-    # my_timer.start()
-    # time.sleep(3)
-    # print("Stopping timer")
-    # my_timer.stop()
-    # time.sleep(3)
-    # print("Reset and start timer 4th time")
-    # my_timer.reset()
-    # my_timer.start()
-    # time.sleep(5.1)
-    # print("Stopping timer")
-    # my_timer.stop()
-    # my_timer.reset()
-    # time.sleep(3)
-    # print("done")
-
 
 if __name__ == "__main__":
     main()
