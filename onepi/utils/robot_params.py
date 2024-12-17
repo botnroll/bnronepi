02161efefb70e9f050a66b957327808ccc336e0d
Nino Pereira <ninopereira.pt@gmail.com>
2024-12-17 21:20:18 +0000

    add content to robotParams class

---
+++ onepi/utils/robot_params.py
@@ -0,0 +1,16 @@
+class RobotParams:
+    """
+    Set of robot parameters
+    """
+    def __init__(self,
+                 max_speed_mmps_in=850,
+                 axis_length_mm_in=163.0,
+                 wheel_diameter_mm_in=65.0,
+                 ticks_per_rev_in=32767):
+        """
+        Defines the robot parameters
+        """
+        self.max_speed_mmps = max_speed_mmps_in
+        self.axis_length_mm = axis_length_mm_in
+        self.wheel_diameter_mm = wheel_diameter_mm_in
+        self.ticks_per_rev = ticks_per_rev_in
