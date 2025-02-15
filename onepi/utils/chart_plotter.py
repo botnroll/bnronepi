bf21349234f99b56662497019f6755c248480c90
Nino Pereira <ninopereira.pt@gmail.com>
2025-02-15 12:47:59 +0000

    feat: improved chart plotter to allow ploting multiple series

--- onepi/utils/chart_plotter.py
+++ onepi/utils/chart_plotter.py
@@ -4,35 +4,60 @@ import time
 
 
 class ChartPlotter:
-    def __init__(self, buffer_size=100, x_label="Iter", y_label="Value"):
-        self.buffer_size = buffer_size
-        self.reference_buffer = np.zeros(buffer_size)
-        self.actual_buffer = np.zeros(buffer_size)
-
+    def __init__(self, number_of_series=1, buffer_size=100, x_label="Iter", y_label="Value", title="Title", labels=[[]]):
+        self.__buffer_size = buffer_size
+        self.__number_of_series = number_of_series
+        self.__serie = [[] for _ in range(number_of_series)]
+        self.__serie_data = [[] for _ in range(number_of_series)]
+        if len(labels) != number_of_series:
+            labels = [[] for _ in range(number_of_series)]
         # Create the plot
         self.fig, self.ax = plt.subplots()
-        self.x = np.arange(buffer_size)
-        (self.line1,) = self.ax.plot(
-            self.x, self.reference_buffer, label="Reference Value"
-        )
-        (self.line2,) = self.ax.plot(self.x, self.actual_buffer, label="Actual Value")
-
+        self.x = np.arange(-buffer_size, 0, 1)
+        for i in range(number_of_series):
+            self.__serie_data[i] = np.zeros(buffer_size)
+            (self.__serie[i],) = self.ax.plot(
+                self.x, self.__serie_data[i], label=labels[i]
+            )
         self.ax.set_ylim(0, 800)
         self.ax.legend()
-        self.ax.set_title("Reference vs. Actual Values")
+        self.ax.set_title(title)
         self.ax.set_xlabel(x_label)
         self.ax.set_ylabel(y_label)
         self.ax.grid(True)
 
-    def update_buffers(self, reference, actual):
-        self.reference_buffer = np.roll(self.reference_buffer, -1)
-        self.actual_buffer = np.roll(self.actual_buffer, -1)
-        self.reference_buffer[-1] = reference
-        self.actual_buffer[-1] = actual
+    def set_title(self, title):
+        self.ax.set_title(title)
 
+    def set_axis_labels(self, x_label, y_label):
+        self.ax.set_xlabel(x_label)
+        self.ax.set_ylabel(y_label)
+
+    def set_series_labels(self, series_labels):
+        if len(series_labels) != self.__number_of_series:
+            print("Error. number of labels != number of series")
+        else:
+            for i in range(self.__number_of_series):
+                self.__serie[i].set_label(series_labels[i])
+            self.ax.legend()
+
+    def set_y_limits(self, y_min = 0, y_max = 800):
+        self.ax.set_ylim(y_min, y_max)
+
+    def update_buffers(self, data):
+        self.x = np.roll(self.x, -1)
+        self.x[-1] = self.x[-2] + 1
+        
+        for i in range(self.__number_of_series):
+            self.__serie_data[i] = np.roll(self.__serie_data[i], -1)
+            self.__serie_data[i][-1] = data[i]    
+            self.__serie[i].set_xdata(self.x)
+            self.__serie[i].set_ydata(self.__serie_data[i])
+        
         # Update the plot
-        self.line1.set_ydata(self.reference_buffer)
-        self.line2.set_ydata(self.actual_buffer)
+        x_limits = self.ax.get_xlim()
+        new_x_limits = (x_limits[0] + 1, x_limits[1] + 1)
+        self.ax.set_xlim(new_x_limits)
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
@@ -46,16 +71,23 @@ class ChartPlotter:
 
 # Example of using the class
 def main():
-    plotter = ChartPlotter()
-    plotter.show_plot()
+    chart_plotter = ChartPlotter(2)
+    chart_plotter.set_y_limits(0, 20)
+    chart_plotter.set_title("Temperature over Time")
+    chart_plotter.set_axis_labels("Time (x0.1 s)", "Temperature (C)")
+    chart_plotter.set_series_labels(["Reference", "Current Temperature"])
+    chart_plotter.show_plot()
 
     for i in range(200):
-        reference_value = np.sin(i * 0.1)  # Example reference data
-        actual_value = np.sin(i * 0.1) + np.random.normal(
-            0, 0.1
+        reference_value = 15 + np.sin(i * 0.1)  # Example reference data
+        actual_value = reference_value + np.random.normal(
+            0, 0.5
         )  # Example actual data with noise
-        plotter.update_buffers(reference_value, actual_value)
+        chart_plotter.update_buffers([reference_value, actual_value])
+        print(reference_value, actual_value)
         time.sleep(0.1)  # Simulation of your timer
+    
+    time.sleep(2)
 
 
 if __name__ == "__main__":
