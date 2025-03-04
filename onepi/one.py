4f7e96076c2d188b823b6a7aa8dfc12f672c0cdb
Trifasico <nandinh99@gmail.com>
2025-03-04 23:52:41 +0000

    fix: Change to behave like in Arduino

--- onepi/one.py
+++ onepi/one.py
@@ -562,7 +562,8 @@ class BnrOneAPlus:
         :return: voltage (V) of input power supply
         :rtype: float
         """
-        return self.__request_word(self._COMMAND_BAT_READ) / 50.7
+        battery = self.__request_word(self._COMMAND_BAT_READ) / 50.7
+        return battery if battery > 0 else 0
 
     def read_left_encoder(self):
         """
